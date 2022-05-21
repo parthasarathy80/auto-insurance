7f9221a4ae5cbbaab2430e096b6b96b1b7860cd5
Parthasarathy Ramaraj <parthasarathyramaraj@Parthasarathys-MacBook-Pro.local>
2022-05-20 22:47:34 -0400

    fixed test cases

--- src/test/java/com/auto/spring/jdbc/services/QuotesServicesTest.java
+++ src/test/java/com/auto/spring/jdbc/services/QuotesServicesTest.java
@@ -1,59 +1,66 @@
-/**
- * 
- */
 package com.auto.spring.jdbc.services;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 import java.text.ParseException;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.auto.spring.jdbc.model.Quote;
+import com.auto.spring.jdbc.repository.JdbcQuoteRepository;
 import com.auto.spring.jdbc.repository.QuoteRepository;
 
 /**
  * @author
  *
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class QuotesServicesTest {
 
-	@MockBean
-	QuoteRepository quoteRepository;
+	@Mock
+	JdbcQuoteRepository quoteRepository;
 
-	@InjectMocks
-	public QuotesServicesImpl quotesServices;
-	
+	@Mock
+	QuoteRepository quotRepository;
+
+	private QuotesServicesImpl quotesServices;
+
+	@BeforeEach
+	public void setup() {
+		quotesServices = new QuotesServicesImpl(quotRepository);
+	}
+
 	@Test
-	public void getSaveQuotesTest() throws ParseException {
+	void getSaveQuotesTest() throws ParseException {
 
 		Quote quote = new Quote();
 		quote.setName("Jeff");
 		quote.setQuote(200);
-		quote.setDob("02-12-1990");
-		quote.setId(1);
+		quote.setDob("20-12-2000");
 
-		Mockito.when(quoteRepository.save(Mockito.any(Quote.class))).thenReturn(1);
 		int quoteID = quotesServices.saveQuotes(quote);
-		//Quote quoteCheck=quoteRepository.findById((long)quoteID);
-		
-		assertEquals(quoteID, quote.getId());
+		Assertions.assertNotNull(quoteID);
+		Assertions.assertEquals(quoteID, quote.getId());
+
+	}
+
+	@Test
+	public void getPremiumTest() throws ParseException {
+		Quote quote= new Quote();
+		quote.setName("Jeff");
+		quote.setQuote(200);
+		quote.setDob("20-12-2000");
+
+		double premium=quotesServices.getPremium(quote);
+		Assertions.assertEquals(premium,90106.51706790138);
+
 
 	}
 
 }
+
+
+
